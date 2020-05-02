6426e7000510d283dcc58c1f714bcf79fbcb60af
MinPro <mbs0722@gmail.com>
2020-05-02 19:40:39 -0400

    Update calculator.js

--- ExpressExamples/calculator.js
+++ ExpressExamples/calculator.js
@@ -4,8 +4,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded(extended : true));
+app.use(bodyParser.urlencoded({extended : true}));
 
+const port = 3000;
+
+app.get("/", function(req, res) { 
+    res.sendFile(__dirname + "/index.html")
+    });
+
+app.post("/", function(req, res) {
+    
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2;
+    
+    res.send("The result is " + result);
+    });
+
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res){
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+
+    let bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
 const port = 3000;
 
 app.get("/", function(req, res) { 
@@ -20,6 +50,9 @@ app.post("/", function(req, res){
   
   res.send("The result is " + result);
 });
+
+app.get("/bmiCalculator", function(req,res){}
+
 
 app.listen(port, function(){ 
   console.log(`Server started on port ${port}`)
