cc3c894895e78fbc35b52820a651822facefbfbe
MinPro <mbs0722@gmail.com>
2020-05-02 18:41:41 -0400

    Update calculator.js

--- ExpressExamples/calculator.js
+++ ExpressExamples/calculator.js
@@ -1,11 +1,24 @@
 //jshint esversion : 6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded(extended : true));
 
 const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(__dirname + "index.html"));
+app.get("/", function(req, res) { 
+  res.sendFile(__dirname + "index.html")
+});
+
+app.post("/", function(req, res){
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  
+  let result = num1 + num2;
+  
+  res.send("The result is " + result);
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
